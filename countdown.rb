60b9188365ac6de2af83360667cd84924ef17dc1
Kwane Smith <smithkwane6@gmail.com>
2020-01-27 19:03:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(secs)
+  secs = 10
+  until secs == 0
+    puts "#{secs} SECONDS(S)!"
+    secs -= 1
+  end
+    "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(secs)
+  secs = 10
+  while secs > 0
+    puts "#{secs} SECONDS(S)!"
+    sleep(1)
+    secs -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
